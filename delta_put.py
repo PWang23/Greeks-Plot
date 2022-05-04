11d6299948f81b64b320d4544f1a6c54dd9ef4b4
PWang23 <85100948+PWang23@users.noreply.github.com>
2022-05-04 23:26:50 +0800

    Add files via upload

---
+++ delta_put.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+import math
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+
+#delta plot
+S = np.linspace(25,150,250)
+T = np.linspace(1,250,250)
+S,T=np.meshgrid(S,T)
+
+#X=100 r=4% b=2% sigma=20%
+X=100
+r=0.04
+b=0.02
+sigma=0.2
+t=T/365
+
+d1 = (np.log(S/X)+(b+sigma**2/2)*t)/(sigma*t**0.5)
+delta_put =np.exp((b-r)*t)*(norm.cdf(d1,loc=0,scale=1)-1)
+
+#3D plot
+from mpl_toolkits.mplot3d import Axes3D
+
+fig = plt.figure(figsize=(12,8))
+ax = fig.add_subplot(projection='3d')
+surf = ax.plot_surface(S,T,delta_put,cmap=plt.cm.coolwarm)
+ax.set_xlabel('Asset Price')
+ax.set_ylabel('Days to maturity')
+ax.set_zlabel('delta_put')
+fig.colorbar(surf,shrink=0.5,aspect=5)
+plt.show()
