083e87a34c1a6b39e981ab6d56b7762baf78b7f8
Yuanzhe Wang <yuanzhe.wang23@gmail.com>
2022-09-13 08:05:20 -0400

    Rho

---
+++ Driftless_Theta.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+import math
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+import sys
+np.set_printoptions(threshold=sys.maxsize)
+
+#delta plot
+S = np.linspace(50,150,100)
+T = np.linspace(5,400,100)
+S,T=np.meshgrid(S,T)
+
+#X=100 r=4% b=2% sigma=20%
+X=100
+r=0.04
+b=0.02
+sigma=0.2
+t=T/365
+
+d1 = (np.log(S/X)+(b+sigma**2/2)*t)/(sigma*t**0.5)
+d2 = d1-sigma*t**0.5
+Driftless_Theta = -S*norm.pdf(d1,loc=0,scale=1)*sigma/(2*t**0.5)
+#3D plot
+from mpl_toolkits.mplot3d import Axes3D
+
+fig = plt.figure(figsize=(12,8))
+ax = fig.add_subplot(projection='3d')
+surf = ax.plot_surface(S,T,Driftless_Theta,cmap=plt.cm.coolwarm)
+ax.set_xlabel('Asset Price')
+ax.set_ylabel('Days to maturity')
+ax.set_zlabel('Driftless_Theta')
+fig.colorbar(surf,shrink=0.5,aspect=5)
+plt.show()
---
+++ Rho_call.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+import math
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+import sys
+np.set_printoptions(threshold=sys.maxsize)
+
+#delta plot
+S = np.linspace(50,150,100)
+T = np.linspace(5,400,100)
+S,T=np.meshgrid(S,T)
+
+#X=100 r=4% b=2% sigma=20%
+X=100
+r=0.04
+b=0.02
+sigma=0.2
+t=T/365
+
+d1 = (np.log(S/X)+(b+sigma**2/2)*t)/(sigma*t**0.5)
+d2 = d1-sigma*t**0.5
+Rho_call = t*X*norm.cdf(d2,loc=0,scale=1)*np.exp(-r*t)
+#3D plot
+from mpl_toolkits.mplot3d import Axes3D
+
+fig = plt.figure(figsize=(12,8))
+ax = fig.add_subplot(projection='3d')
+surf = ax.plot_surface(S,T,Rho_call,cmap=plt.cm.coolwarm)
+ax.set_xlabel('Asset Price')
+ax.set_ylabel('Days to maturity')
+ax.set_zlabel('Rho_call')
+fig.colorbar(surf,shrink=0.5,aspect=5)
+plt.show()
