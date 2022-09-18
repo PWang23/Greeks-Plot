fa0b52376bcdab7f8dff65013a618eab05ffd408
Yuanzhe Wang <yuanzhe.wang23@gmail.com>
2022-09-18 18:21:32 -0400

    Create Vomma.py

---
+++ Vomma.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+import math
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+import sys
+np.set_printoptions(threshold=sys.maxsize)
+
+#delta plot
+S = np.linspace(50,150,100)
+T = np.linspace(1,300,100)
+S,T=np.meshgrid(S,T)
+
+#X=100 r=4% b=2% sigma=20%
+X=100
+r=0.04
+b=0.02
+sigma=0.2
+t=T/365
+
+d1 = (np.log(S/X)+(b+sigma**2/2)*t)/(sigma*t**0.5)
+d2 = d1-sigma*t**0.5
+Vega =S*np.exp((b-r)*t)*norm.pdf(d1,loc=0,scale=1)*t**0.5
+Vomma = Vega*d1*d2/sigma
+
+#3D plot
+from mpl_toolkits.mplot3d import Axes3D
+
+fig = plt.figure(figsize=(12,8))
+ax = fig.add_subplot(projection='3d')
+surf = ax.plot_surface(S,T,Vomma,cmap=plt.cm.coolwarm)
+ax.set_xlabel('Asset Price')
+ax.set_ylabel('Days to maturity')
+ax.set_zlabel('Vomma')
+fig.colorbar(surf,shrink=0.5,aspect=5)
+plt.show()
